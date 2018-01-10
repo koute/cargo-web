koute/cargo-web

commit e50975188282969b140a8648d03f89cdd6d3ffbd

Do not rebuild library crates on Emscripten twice
These don't produce any artifacts besides an `.rlib` which we don't care about.

src/cargo_shim/mod.rs
@@ -101,6 +101,15 @@ pub enum BuildTarget {
     IntegrationBench( String )
 }
 
+impl BuildTarget {
+    fn is_lib( &self ) -> bool {
+        match *self {
+            BuildTarget::Lib( .. ) => true,
+            _ => false
+        }
+    }
+}
+
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum MessageFormat {
     Human,
@@ -221,7 +230,7 @@ impl BuildConfig {
             triplet == "wasm32-unknown-emscripten" || triplet == "asmjs-unknown-emscripten"
         }).unwrap_or( false );
 
-        if is_emscripten {
+        if is_emscripten && !self.build_target.is_lib() {
             let no_js_generated = result
                 .artifacts()
                 .iter()

commit f053eda5f29cfda6d0af30b86f6ddab8eccd021a

Fix the workaround for Emscripten not producing artifacts on fresh install
This was broken by the `e50975188282969b140a8648d03f89cdd6d3ffbd` commit.

src/cargo_shim/mod.rs
@@ -141,10 +141,10 @@ pub enum BuildTarget {
 }
 
 impl BuildTarget {
-    fn is_lib( &self ) -> bool {
+    fn is_executable( &self ) -> bool {
         match *self {
-            BuildTarget::Lib( .. ) => true,
-            _ => false
+            BuildTarget::Lib( _, Profile::Main ) => false,
+            _ => true
         }
     }
 }
@@ -269,7 +269,7 @@ impl BuildConfig {
             triplet == "wasm32-unknown-emscripten" || triplet == "asmjs-unknown-emscripten"
         }).unwrap_or( false );
 
-        if is_emscripten && !self.build_target.is_lib() {
+        if is_emscripten && self.build_target.is_executable() {
             let no_js_generated = result
                 .artifacts()
                 .iter()