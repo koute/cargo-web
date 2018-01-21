24b7642a653f3936cab6032b95689518490214a6
Nathan Essex <nathan@essex.id.au>
2018-01-21 11:34:44 +1000

    Don't rely on system emscripten

--- src/cmd_prepare_emscripten.rs
+++ src/cmd_prepare_emscripten.rs
@@ -2,7 +2,7 @@ use emscripten::initialize_emscripten;
 use error::Error;
 
 pub fn command_prepare_emscripten< 'a >() -> Result< (), Error > {
-    match initialize_emscripten( true, true ) {
+    match initialize_emscripten( false, true ) {
         None => return Err( Error::BuildError ),
         Some( _emscripten ) => return Ok( () ),
     }
