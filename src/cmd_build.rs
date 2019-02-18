f8b6040509da7a34de7f3af2fcb7e58ec10d07d1
George Kaplan <george@georgekaplan.xyz>
2019-02-18 01:07:23 -0500

    attempt to undo formatting

--- src/cmd_build.rs
+++ src/cmd_build.rs
@@ -1,23 +1,24 @@
-use cargo_shim::{Profile, TargetKind};
+use cargo_shim::{
+    Profile,
+    TargetKind
+};
 
 use build::BuildArgs;
 use error::Error;
 
-pub fn command_build_or_check<'a>(build_args: BuildArgs, should_build: bool) -> Result<(), Error> {
+pub fn command_build_or_check(build_args: BuildArgs, should_build: bool) -> Result<(), Error> {
     let project = build_args.load_project()?;
 
     let targets = project.target_or_select(|target| {
-        target.kind == TargetKind::Lib
-            || target.kind == TargetKind::CDyLib
-            || target.kind == TargetKind::Bin
+        target.kind == TargetKind::Lib || target.kind == TargetKind::CDyLib || target.kind == TargetKind::Bin
     })?;
 
-    let config = project.aggregate_configuration(Profile::Main)?;
+    let config = project.aggregate_configuration( Profile::Main )?;
     for target in targets {
         if should_build {
-            project.build(&config, target)?;
+            project.build( &config, target )?;
         } else {
-            project.check(&config, target)?;
+            project.check( &config, target )?;
         }
     }
 
